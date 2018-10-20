cf1a28fa28111e9a846cb71a72e3c22033465da5
Joshua Matthews <jmatth93@gmail.com>
2018-10-20 12:18:29 -0500

    changed gitignore

--- run_game.sh
+++ run_game.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 set -e
-go build main
+go build -o bot main
 
-./halite --replay-directory replays/ -vvv --width 32 --height 32 "./main" "./main2"
+./halite --replay-directory replays/ -vvv --width 32 --height 32 "./bot" "./bot2"
