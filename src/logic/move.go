1c9bb77ab7baa3fca539d8911049e078e808ad1f
Joshua Matthews <jmatth93@gmail.com>
2018-10-20 19:19:58 -0500

    Collision avoidance for future moves
    - ships check other ships future moves
    - random moves currently

--- src/logic/move.go
+++ src/logic/move.go
@@ -22,12 +22,7 @@ func NewGameAI() *GameAI {
 func (gm *GameAI) Move(gMap *hlt.GameMap, ship *hlt.Ship, maxHalite int) hlt.Command {
 	var currentCell = gMap.AtEntity(ship.E)
 	if currentCell.Halite < (maxHalite/10) || ship.IsFull() {
-		dirs := helper.AvailableDirectionsForEntity(gMap, ship.E)
-		if len(dirs) > 0 {
-			choice := dirs[rand.Intn(len(dirs))]
-			gm.MarkFuturePos(gMap, helper.NormalizedDirectionalOffset(ship.E.Pos, gMap, choice))
-			return ship.Move(dirs[rand.Intn(len(dirs))])
-		}
+		return gm.randomAvailableDirection(gMap, ship)
 	}
 	gm.MarkFuturePos(gMap, ship.E.Pos)
 	return ship.Move(hlt.Still())
@@ -37,4 +32,39 @@ func (gm *GameAI) Move(gMap *hlt.GameMap, ship *hlt.Ship, maxHalite int) hlt.Com
 func (gm *GameAI) MarkFuturePos(gMap *hlt.GameMap, pos *hlt.Position) {
 	cell := gMap.AtPosition(pos)
 	gm.FuturePos[pos.String()] = cell
+}
+
+// IsPosClaimed - check to see if position has been claimed as a future spot
+func (gm *GameAI) IsPosClaimed(pos *hlt.Position) bool {
+	_, ok := gm.FuturePos[pos.String()]
+	return ok
+}
+
+func (gm *GameAI) randomAvailableDirection(gMap *hlt.GameMap, ship *hlt.Ship) hlt.Command {
+	dirs := helper.AvailableDirectionsForEntity(gMap, ship.E)
+	choice, nextPos := exhaustOptions(gm, gMap, ship.E.Pos, dirs)
+	gm.MarkFuturePos(gMap, nextPos)
+	return ship.Move(choice)
+}
+
+func exhaustOptions(gm *GameAI, gMap *hlt.GameMap, pos *hlt.Position, dirs []*hlt.Direction) (*hlt.Direction, *hlt.Position) {
+	if len(dirs) > 0 {
+		N := rand.Intn(len(dirs))
+		choice := dirs[N]
+		nextPos := helper.NormalizedDirectionalOffset(pos, gMap, choice)
+		if !gm.IsPosClaimed(nextPos) {
+			return choice, nextPos
+		}
+		for i := 0; i < len(dirs); i++ {
+			if i == N {
+				continue
+			}
+			choice = dirs[N]
+			nextPos = helper.NormalizedDirectionalOffset(pos, gMap, choice)
+			if !gm.IsPosClaimed(nextPos) {
+				return choice, nextPos
+			}
+		}
+	}
+	return hlt.Still(), pos
 }
